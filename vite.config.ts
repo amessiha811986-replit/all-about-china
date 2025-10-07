7f64ee7802d4416d61516280bc17759d1d12ba22
amessiha811986-replit <alexander.messiha@outlook.com>
2025-10-07 10:02:33 -0400

    updated to react 19.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwind from "@tailwindcss/vite";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -22,7 +23,8 @@ export default defineConfig({
       appDirectory: "src"
     }),
 
-    tsconfigPaths()
+    tsconfigPaths(),
+    tailwind()
   ],
 
   server: {
